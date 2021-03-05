36483a93ea454f05766ea403425545356debdb24
tommy1011 <thomas.reilly@ucdconnect.ie>
2021-03-05 13:24:43 +0000

    Changed package name of UserController and added empty entity clasees to avoid future merge conflicts.

--- src/main/java/app/controllers/UserController.java
+++ src/main/java/app/controllers/UserController.java
@@ -1,4 +1,4 @@
-package com.example.springboot;
+package app.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
