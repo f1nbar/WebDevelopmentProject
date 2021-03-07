f7c56b4b136f90a3756bfe2ef8188604b6a36b38
Finbar Ó Deaghaidh <finbar@tutanota.com>
2021-03-07 13:05:45 +0000

    Products Skeleton

--- src/main/java/app/entity/Product.java
+++ src/main/java/app/entity/Product.java
@@ -1,4 +1,48 @@
 package app.entity;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "product")
 public class Product {
+
+    @Id
+    @GeneratedValue(generator = "SkuGenerator", strategy = GenerationType.IDENTITY)
+    @GenericGenerator(name = "SkuGenerator", strategy = "app.entity.SkuGenerator")
+    protected String id;
+    private String productName;
+    private int price;
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public boolean isVisible() {
+        return isVisible;
+    }
+
+    public void setVisible(boolean visible) {
+        isVisible = visible;
+    }
+
+    private boolean isVisible;
+
+    public Product(){
+
+    }
+
 }
